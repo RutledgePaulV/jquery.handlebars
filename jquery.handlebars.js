f544aa57c545ee708cb06fe4dbb10b7cf56384b0
paul.rutledge <paul.rutledge@vodori.com>
2014-10-26 13:28:49 -0500

    updated the plugin to not override Handlebars.templates

--- jquery.handlebars.js
+++ jquery.handlebars.js
@@ -3,12 +3,7 @@
  * To use it, specify a data-template attribute on the dom element that should act as the rendering
  * frame. This attribute should provide a URI at which the template can be accessed. If the template
  * has not already been precompiled onto the handlebars object, a get request will be created to fetch
- * the template and then compile it.
- *
- * Once the compiled template is available, it's render function will be modified to automatically
- * place the rendered contents in the pane to which the data-template attribute was attached. If you
- * do not wish to render all areas that use the template, you can pass a selector as the second argument
- * to the render function and it will only render those panes which match the selector.
+ * the template and then compile it, or just execute if a precompiled template.
  *
  * The idea behind handling things this way, is that it leaves the presentation layer without the markup
  * but gives you full control over the rendering inside the JS. The intention is that this will promote
@@ -29,7 +24,7 @@
         return nameOrPath.substr(nameOrPath.lastIndexOf('.'));
     }
 
-    function getFileName(uri){
+    function getTemplateName(uri){
         var name = getFileNameWithExtension(uri);
         var extension = getFileExtension(name);
         return name.replace(extension, '');
@@ -41,7 +36,7 @@
      * they were assigned.
      *
      *
-     * @param {string} [uriPrefix] An optional path prefix to append to each
+     * @param {string} [uriPrefix] An optional path prefix to prepend to each
      *                             encountered path in the data-template attribute.
      */
     $.handlebars = function (uriPrefix) {
@@ -57,8 +52,9 @@
             // getting the path to the template to pull in with an optional global prefix
             var templateUri = uriPrefix + $(this).data('template');
 
-            // getting the name to place the template under
-            var handlebarsPrefix = getFileName(templateUri);
+            var extension = getFileExtension(templateUri);
+            var handlebarsPrefix = getTemplateName(templateUri);
+
 
             // keeping track of the elements that have templates associated with them, and which template
             if (!TemplateElements.hasOwnProperty(handlebarsPrefix)) {
@@ -67,51 +63,39 @@
                 TemplateElements[handlebarsPrefix].push(this);
             }
 
-            //optionally requesting the script and compiling it if it doesn't already exist.
+            // requesting and compiling the template if the filename had a handlebars extension
+            // otherwise request and execute as a script if js extension (precompiled)
             if (!Handlebars.templates.hasOwnProperty(handlebarsPrefix)) {
-                // loading and compiling script if it doesn't already exist on the Handelbars object
-                // and then overriding its default behavior once it's compiled.
-                $.get(templateUri).done(function (contents) {
-                    Handlebars.templates[handlebarsPrefix] = Handlebars.compile(contents);
-                    override(handlebarsPrefix);
-                });
-            } else {
-
-                //if it already exists, we can just override the behavior - it must have been precompiled.
-                override(handlebarsPrefix);
+                if(extension === 'handlebars'){
+                    $.get(templateUri).done(function (contents) {
+                        Handlebars.templates[handlebarsPrefix] = Handlebars.compile(contents);
+                    });
+                } else if(extension === 'js'){
+                    $.getScript(templateUri);
+                }
             }
         });
     };
 
-
     /**
-     * This overrides a particular handlebars template's rendering function to be scoped to only
-     * those elements for which it was defined and also makes the rendering function filterable
-     * against those elements.
+     * This function wraps the normal handlebars template function in a way
+     * that allows us to scope it specifically to the elements to which the
+     * data-template attribute was provided.
      *
-     * @param {string} templateName The name of the template to be overridden.
+     * @param {string} templateName The name of the template to render.
+     * @param {object} data The data to render the template with.
+     * @param {string} selector A css selector to identify the element of the available set. use ('*') for all.
+     * @returns {null|string} Returns null if a selector was provided, else returns the rendered html.
      */
-    function override(templateName) {
-
-        // capturing the original rendering function
-        var originalCompileFunc = Handlebars.templates[templateName];
-
-        /**
-         * We're overriding the default handlebars renderer to accept both the template context
-         * as well as a selector for the available elements for which it should be rendered at
-         * any given time. This way elements can share templates but be rendered independently.
-         *
-         * @param {object} context An object with keys and values to render the template with.
-         *
-         * @param {string|function():boolean} selector A selector to decide which of the available
-         *                                             elements should have the newly rendered template
-         *                                             applied.
-         */
-        Handlebars.templates[templateName] = function (context, selector) {
-            $(TemplateElements[templateName]).filter(selector || '*').each(function (index, element) {
-                $(element).html(originalCompileFunc(context));
+    $.renderHandlebars = function(templateName, data, selector){
+        var rendered = Handlebars.templates[templateName](data);
+        if(!selector){
+            return rendered;
+        } else {
+            $(TemplateElements[templateName]).filter(selector).each(function (index, element) {
+                $(element).html(rendered);
             });
         }
-    }
+    };
 
 }(jQuery));
